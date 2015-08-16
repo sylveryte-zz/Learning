48a56caf94b32a92ecda958dd82a03543f691aa9
sylveryte <sylveryte@gmail.com>
2015-08-16 16:57:03 +0530

    sz

--- C/pget.c
+++ C/pget.c
@@ -2,9 +2,10 @@
 main()
 {
 	int d[]={3,4,6,24,};
+	int m;
 	char c[4];
 	if(d[3]==3[d])
-		printf("\nYo same\n");
+		printf("\nsizeof d : %i\n",sizeof(m));
 	printf("Enter string : ");
 	scanf("%3s",c);
 	printf("String : %s",c);
