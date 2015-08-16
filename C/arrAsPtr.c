292ec40d90d5043567651437132f2a89b109addc
sylveryte <sylveryte@gmail.com>
2015-08-16 16:07:49 +0530

    string memory & strcoy conflict

--- C/arrAsPtr.c
+++ C/arrAsPtr.c
@@ -6,17 +6,32 @@
 int sytrlen(char* );
 void sytrcpy(char* ,char* );
 
+void strcp(char* o,char* c)
+{
+	while(*o!='\0')
+	{
+		*c=*o;
+		c++;
+		o++;
+	}
+		
+}
+
 main()
 {
 	char original[20];
 	char copied[20];
+	char copieded[3];                    //less size  try greater size too
 	printf("\nEnter string : ");
 	gets(original);
 
 	printf("Original : %s",original);
 	printf("\nLength : %d",sytrlen(original));
 	sytrcpy(original,copied);
-	printf("\nCopied : %s\nYo\n",copied);
+	printf("\nCopied : %s\n",copied);
+	strcp(original,copieded);
+	printf("\nCopiedMine : %s\nYo\n",copieded);
+	
 	return 0;
 }
 
@@ -25,6 +40,7 @@ void sytrcpy(char* o,char* c)
 	while((*c++=*o++)!=0)
 		;
 }
+
 
 int sytrlen(char* c)
 {
---
+++ C/pget.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int d[]={3,4,6,24,};
+	char c[4];
+	if(d[3]==3[d])
+		printf("\nYo same\n");
+	printf("Enter string : ");
+	scanf("%3s",c);
+	printf("String : %s",c);
+	return 0;
+}
